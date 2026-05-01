c3f14cdb66c8087b963e9e2d22ff39f36c4e4585
MD ASAD <mdasadgodargama@gmail.com>
2026-05-01 23:25:13 +0530

    Implemented bit manipulation concepts in Java: bitwise operators

---
+++ Bit-Manipulation/BitOperators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class BitOperators{
+    public static void main(String arg[]){
+        // Binary AND
+        System.out.println((5 & 6));
+        //  Binary OR
+        System.out.println((5 | 6));
+        // Binary XOR
+        System.out.println((5 ^ 6));
+        //  Binary One's Complement
+        System.out.println((~5));
+        //  Binary One's Complement
+        System.out.println((~0));
+        //  Binary Left Shift
+        System.out.println((5<<2));
+        // Binary Right Shift
+        System.out.println((6>>1));
+    }
+}
