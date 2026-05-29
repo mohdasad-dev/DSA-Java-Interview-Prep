5c969cd669a1621bfd00642f578c61c06009a55c
MD ASAD <mdasadgodargama@gmail.com>
2026-05-29 20:11:15 +0530

    Implemented recursion programs in Java to calculate factorial of N and sum of first N natural numbers

---
+++ Recursion/SumOfNaturalNumbers.java
@@ -0,0 +1,14 @@
+public class SumOfNaturalNumbers{
+    public static int calcSum(int n){
+        if(n == 1){
+            return 1;
+        }
+        int Snm1 = calcSum(n-1);
+        int Sn = n + Snm1;
+        return Sn;
+    }
+    public static void main(String arg[]){
+        int n = 5;
+        System.out.println(calcSum(n));
+    }
+}
